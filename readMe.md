33b247d97c2b7466b496af59d242a25c96e512dc
gauravkharel <kharel619@gmail.com>
2024-01-13 14:08:47 +0545

    Documenting api

---
+++ readMe.md
@@ -0,0 +1,5 @@
+## Resources or, just copied from
+https://www.freecodecamp.org/news/rest-api-design-best-practices-build-a-rest-api/#architecture
+
+https://restfulapi.net/security-essentials/
+
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,9 +1,101 @@
 const express = require("express");
-const router = express.Router();
+
+const apicache = require('apicache')
 const workoutController = require("../../controllers/workoutController");
+const recordController = require("../../controllers/recordController");
+const router = express.Router();
+
+const cache = apicache.middleware;
+// const authenticate = require("../../middlewares/authenticate");
+// const authorize = require("../../middlewares/authorize");
+
+// router.post("/", authenticate, authorize, workoutController.createNewWorkout);
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Workout:
+ *       type: object
+ *       properties:
+ *         id: 
+ *           type: string
+ *           example: 61dbae02-c147-4e28-863c-db7bd402b2d6
+ *         name: 
+ *           type: string
+ *           example: Tommy V  
+ *         mode:
+ *           type: string
+ *           example: For Time
+ *         equipment:
+ *           type: array
+ *           items:
+ *             type: string
+ *           example: ["barbell", "rope"]
+ *         exercises:
+ *           type: array
+ *           items:
+ *             type: string
+ *           example: ["21 thrusters", "12 rope climbs, 15 ft", "15 thrusters", "9 rope climbs, 15 ft", "9 thrusters", "6 rope climbs, 15 ft"]
+ *         createdAt:
+ *           type: string
+ *           example: 4/20/2022, 2:21:56 PM
+ *         updatedAt: 
+ *           type: string
+ *           example: 4/20/2022, 2:21:56 PM
+ *         trainerTips:
+ *           type: array
+ *           items:
+ *             type: string
+ *           example: ["Split the 21 thrusters as needed", "Try to do the 9 and 6 thrusters unbroken", "RX Weights: 115lb/75lb"]
+ */
 
 
-router.get("/",  workoutController.getAllWorkouts);
+router.get("/", cache('2 minutes'),  workoutController.getAllWorkouts);
 
 router.get("/:workoutId", (req, res) => {
   res.send("Get an existing workout");
@@ -21,12 +113,12 @@ router.delete("/:workoutId", (req, res) => {
   res.send("Delete an existing workout");
 });
 
-router.get("/:workoutId/records", recordController.getRecordForWorkout);
+// router.get("/:workoutId/records", recordController.getRecordForWorkout);
 
-router.post("/", workoutController.createNewWorkout);
+// router.post("/", workoutController.createNewWorkout);
 
-router.patch("/:workoutId", workoutController.updateOneWorkout);
+// router.patch("/:workoutId", workoutController.updateOneWorkout);
 
-router.delete("/:workoutId", workoutController.deleteOneWorkout);
+// router.delete("/:workoutId", workoutController.deleteOneWorkout);
 
 module.exports = router;
